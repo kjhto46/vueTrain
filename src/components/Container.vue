ae1d96a18c68493ef06ba9f48a6a0cb3272a94b6
kjhto46 <tony1996kjh@gmail.com>
2023-11-18 21:08:07 +0900

    component 만들고 v-for을 이용해 반복, 데이터 전송

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    컨테이너예요
+    <Post v-for="(a,i) in instarData" :key="a" :instarData="instarData[i]" />
+  </div>
+</template>
+<script>
+import Post from './Post.vue'
+export default {
+  name : 'ContainerI',
+  components : {
+    Post,
+  },
+  props : {
+    instarData : Object,
+  }
+}
+</script>
+<style>
+  
+</style>
